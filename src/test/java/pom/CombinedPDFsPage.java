61ad807026ef6c5747635f91f3be0b9e9b2b1fe2
Meghana <meghana.chappidi@centricconsulting.com>
2017-07-14 16:42:17 -0400

    Added new web elements related to list result count

--- src/test/java/pom/CombinedPDFsPage.java
+++ src/test/java/pom/CombinedPDFsPage.java
@@ -1,5 +1,32 @@
 package pom;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class CombinedPDFsPage {
 
+	WebDriver driver;
+	public  CombinedPDFsPage(WebDriver driver) {
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+	
+	}
+	
+		@FindBy(className="resultsCount")
+		WebElement resultCount;
+		
+		
+		public WebElement getResultCount() {
+			return resultCount;
+		}
+
+		public void setResultCount(WebElement resultCount) {
+			this.resultCount = resultCount;
+		}
+		
+		
+	
+	
 }
