c299191a6433580c66afe9186669563717f42dde
Meghana <meghana.chappidi@centricconsulting.com>
2017-07-14 16:46:22 -0400

    Added code for API smoke test scenario

--- src/test/java/stepDefinition/scr_050a_files_list.java
+++ src/test/java/stepDefinition/scr_050a_files_list.java
@@ -2,19 +2,26 @@ package stepDefinition;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 
+import javax.json.JsonObject;
+
+import org.apache.commons.lang.StringUtils;
 import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
-
 import cucumber.api.DataTable;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
+import pom.FilesPage;
 import pom.MenuSubMenuPage;
+import utils.APICall;
+import utils.APIUtils;
 import utils.BrowserFactory;
 import utils.BrowserInteractions;
+import utils.PropertiesLoad;
 
 public class scr_050a_files_list {
 	
@@ -41,6 +48,21 @@ public class scr_050a_files_list {
 			headerListUI.add(columns.get(cnum).getText());
 		}
 		Assert.assertEquals(headerList, headerListUI);
+	}
+	
+	@Then("^the Files result count will be as expected$")
+	public void the_Files_result_count_will_be_as_expected() throws Throwable {
+		Properties properties = PropertiesLoad.loadFromFile("config.properties");
+		String APIURL = properties.getProperty("api.url");
+		String token = APIUtils.getToken();
+		String filesRequestParameters = "{\"language\":\"ENGLISH\",\"fileSearchCriteria\":{\"groupDescriptor\":{\"dbName\":\""+properties.getProperty("dbName")+"\",\"groupName\":\""+properties.getProperty("groupName")+"\"},\"startPage\":1,\"pageSize\":20}}";
+		String filesResponse = APICall.getResponse(APIURL+"/jobprocessing/getFiles?token="+token, filesRequestParameters);
+		JsonObject object = APIUtils.getJSONObjectFromResponse(filesResponse, "processedFileInfoResults");
+		String APIRowCount = APIUtils.getValueFromResponse(object.toString(), "count", "int");
+		
+		FilesPage filesPage = new FilesPage(driver);
+		String UIRowCount = filesPage.getResultCount().getText().split("of")[1].trim();
+		Assert.assertTrue("API count "+APIRowCount+" not matched UI count"+UIRowCount, StringUtils.equals(APIRowCount, UIRowCount));
 	}
 	
 }
