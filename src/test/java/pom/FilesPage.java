58b2284ca1732cdd6beffc344e3a427c5038f166
Meghana <meghana.chappidi@centricconsulting.com>
2017-07-14 16:49:56 -0400

    Added new class for Files page

---
+++ src/test/java/pom/FilesPage.java
@@ -0,0 +1,32 @@
+package pom;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class FilesPage {
+
+	WebDriver driver;
+	public  FilesPage(WebDriver driver) {
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+	
+	}
+	
+		@FindBy(className="resultsCount")
+		WebElement resultCount;
+		
+		
+		public WebElement getResultCount() {
+			return resultCount;
+		}
+
+		public void setResultCount(WebElement resultCount) {
+			this.resultCount = resultCount;
+		}
+		
+		
+	
+	
+}
