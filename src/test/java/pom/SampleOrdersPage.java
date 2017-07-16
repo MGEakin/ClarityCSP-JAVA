bccd0028bf4b617adaae8ad9fde1b49f176e7ab4
Meghana <meghana.chappidi@centricconsulting.com>
2017-07-16 14:28:13 -0400

    Added new Web elements for added new scenarios

--- src/test/java/pom/SampleOrdersPage.java
+++ src/test/java/pom/SampleOrdersPage.java
@@ -1,6 +1,8 @@
 package pom;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class SampleOrdersPage {
@@ -11,5 +13,38 @@ public class SampleOrdersPage {
 		PageFactory.initElements(driver, this);
 	}
 	
+	@FindBy(className = "resultsCount")
+	WebElement resultsCount;
+	
+	@FindBy(id="addressBookButton")
+	WebElement addressBookButton;
+	
+	@FindBy(id="sampleorders_sort_123")
+	WebElement sampleordersSort;
+	
+	public WebElement getResultsCount() {
+	return resultsCount;
+	}
 
+	public void setResultsCount(WebElement resultsCount) {
+		this.resultsCount = resultsCount;
+	}
+
+	public WebElement getAddressBookButton() {
+		return addressBookButton;
+	}
+
+	public void setAddressBookButton(WebElement addressBookButton) {
+		this.addressBookButton = addressBookButton;
+	}
+
+	public WebElement getSampleordersSort() {
+		return sampleordersSort;
+	}
+
+	public void setSampleordersSort(WebElement sampleordersSort) {
+		this.sampleordersSort = sampleordersSort;
+	}
+
+	
 }
