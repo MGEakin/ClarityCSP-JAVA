1e0dcb08b0ed100a7799b583bcbe6582ed1fab15
Meghana <meghana.chappidi@centricconsulting.com>
2017-07-18 09:32:38 -0400

    Added new web elements

--- src/test/java/pom/OrdersPage.java
+++ src/test/java/pom/OrdersPage.java
@@ -16,9 +16,60 @@ public class OrdersPage {
 	@FindBy(id="processed_date_pulldown")
 	WebElement processedDatePulldown;
 	
-	@FindBy(id = "searchButton")
-	WebElement searchButton;
+	@FindBy(xpath=".//*[@id='orders']/thead/tr")
+	WebElement ordersListId;
 	
+	@FindBy(xpath="html/body/app-root/div[3]/members/dynamic-table/div/div[1]")
+	WebElement membersResultCount;
+	
+	@FindBy(xpath=".//*[@id='ordersDiv']/dynamic-table/div/div[1]")
+	WebElement ordersResultCount;
+	
+	@FindBy(xpath="html/body/app-root/div[3]/members/div/div")
+	WebElement GroupID;
+	
+	
+	@FindBy(id="State")
+	WebElement State;
+	
+
+	@FindBy(id="searchButton1")
+	WebElement searchButton1;
+	
+	
+	
+	public WebElement getGroupID() {
+		return GroupID;
+	}
+
+	public void setGroupID(WebElement groupID) {
+		GroupID = groupID;
+	}
+
+	public WebElement getOrdersResultCount() {
+		return ordersResultCount;
+	}
+
+	public void setOrdersResultCount(WebElement ordersResultCount) {
+		this.ordersResultCount = ordersResultCount;
+	}
+
+	public WebElement getMembersResultCount() {
+		return membersResultCount;
+	}
+
+	public void setMembersResultCount(WebElement membersResultCount) {
+		this.membersResultCount = membersResultCount;
+	}
+
+	public WebElement getOrdersListId() {
+		return ordersListId;
+	}
+
+	public void setOrdersListId(WebElement ordersListId) {
+		this.ordersListId = ordersListId;
+	}
+
 	public WebElement getProcessedDatePulldown() {
 		return processedDatePulldown;
 	}
@@ -27,16 +78,19 @@ public class OrdersPage {
 		this.processedDatePulldown = processedDatePulldown;
 	}
 
-	public WebElement getSearchButton() {
-		return searchButton;
+	public WebElement getSearchButton1() {
+		return searchButton1;
 	}
 
-	public void setSearchButton(WebElement searchButton) {
-		this.searchButton = searchButton;
+	public void setSearchButton1(WebElement searchButton1) {
+		this.searchButton1 = searchButton1;
 	}
 
+	public WebElement getState() {
+		return State;
+	}
 
-
-	
-	
+	public void setState(WebElement state) {
+		State = state;
+	}
 }
