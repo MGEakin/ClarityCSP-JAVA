7148f673f7d9693f0ba9e0305eab3a59637c8f8b
Meghana <meghana.chappidi@centricconsulting.com>
2017-07-21 11:30:45 -0400

    Added new features to test

--- src/test/java/test/TestRunner.java
+++ src/test/java/test/TestRunner.java
@@ -11,6 +11,13 @@ import cucumber.api.junit.Cucumber;
 //@CucumberOptions(features = "src/test/java/features/orders/scr_055b_orders_list.feature",
 //@CucumberOptions(features = "src/test/java/features/overrides/scr_073a_overrides_list.feature",
 //@CucumberOptions(features = "src/test/java/features/sample_orders/scr_098a_sample_order_list.feature",
+//@CucumberOptions(features = "src/test/java/features/errors/scr_052_errors_list.feature",
+//@CucumberOptions(features = "src/test/java/features/notices/scr_085_manage_notice_messages_list.feature",
+//@CucumberOptions(features = "src/test/java/features/admincontacts/scr_087_manage_contact_list.feature",
+//@CucumberOptions(features = "src/test/java/features/admincontacts/scr_088_edit_and_create_contacts.feature",
+//@CucumberOptions(features = "src/test/java/features/errors/scr_052_errors_list.feature",
+//@CucumberOptions(features = "src/test/java/features/errors/scr_133_view_report_window.feature",
+
 glue ={"stepDefinition"})
 public class TestRunner {
 
