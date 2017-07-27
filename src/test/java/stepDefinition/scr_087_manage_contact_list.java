c674e29c67c7c90a090c3ffdb6851f71b7802e77
Meghana <meghana.chappidi@centricconsulting.com>
2017-07-27 13:40:28 -0400

    Added step definitions for the new scenarios in manage contacts list page

--- src/test/java/stepDefinition/scr_087_manage_contact_list.java
+++ src/test/java/stepDefinition/scr_087_manage_contact_list.java
@@ -94,14 +94,20 @@ public class scr_087_manage_contact_list {
 		List<List<String>> data = dataTable.raw();
 		Thread.sleep(3000);
 		adminContacts = new AdminContactsPage(driver);
-		Assert.assertEquals("Data mismatch in Support Role", data.get(1).get(0).trim(), (adminContacts.getSupportRole().getText().trim()));
-		Assert.assertEquals("Data mismatch in Contact Company", data.get(1).get(1).trim(), (adminContacts.getContactCompany().getText().trim()));
-		Assert.assertEquals("Data mismatch in Contact Name", data.get(1).get(2).trim(), (adminContacts.getContactName().getText().trim()));
-		Assert.assertEquals("Data mismatch in Contact EmailId", data.get(1).get(3).trim(), (adminContacts.getContactEmailId().getText().trim()));
-		Assert.assertEquals("Data mismatch in Contact PhoneNo", data.get(1).get(4).trim(), (adminContacts.getContactPhoneNo().getText().trim()));
-		Assert.assertEquals("Data mismatch in Home Display", data.get(1).get(5).trim(), (adminContacts.getHomeDisplay().getText().trim()));
-		Assert.assertEquals("Data mismatch in Actions Elements", data.get(1).get(6).replaceAll("\\s",""), (adminContacts.getActionsElements().getText().replaceAll("\\r|\\n|\\s", "")));
-			
+		Assert.assertEquals("Data mismatch in Support Role", data.get(1).get(0).trim(),
+				(adminContacts.getSupportRole().getText().trim()));
+		Assert.assertEquals("Data mismatch in Contact Company", data.get(1).get(1).trim(),
+				(adminContacts.getContactCompany().getText().trim()));
+		Assert.assertEquals("Data mismatch in Contact Name", data.get(1).get(2).trim(),
+				(adminContacts.getContactName().getText().trim()));
+		Assert.assertEquals("Data mismatch in Contact EmailId", data.get(1).get(3).trim(),
+				(adminContacts.getContactEmailId().getText().trim()));
+		Assert.assertEquals("Data mismatch in Contact PhoneNo", data.get(1).get(4).trim(),
+				(adminContacts.getContactPhoneNo().getText().trim()));
+		Assert.assertEquals("Data mismatch in Home Display", data.get(1).get(5).trim(),
+				(adminContacts.getHomeDisplay().getText().trim()));
+		Assert.assertEquals("Data mismatch in Actions Elements", data.get(1).get(6).replaceAll("\\s", ""),
+				(adminContacts.getActionsElements().getText().replaceAll("\\r|\\n|\\s", "")));
 	}
 
 	@When("^I select the Manage Contact - Edit action$")
@@ -120,27 +126,35 @@ public class scr_087_manage_contact_list {
 
 	}
 
-	 @Then("^the following Edit and Create Contacts row content will be correct$")
-	 public void the_following_Edit_and_Create_Contacts_row_content_will_be_correct(DataTable dataTable) throws Throwable {
-	 List<List<String>> data = dataTable.raw();
-	 adminContacts = new AdminContactsPage(driver);
-	 Thread.sleep(5000);
-	 Assert.assertEquals("Data mismatch in Contact Name",data.get(1).get(0),adminContacts.getEditPageContactName().getAttribute("value"));
-	 Assert.assertEquals("Data mismatch in Contact Email",data.get(1).get(1),adminContacts.getEditPageContactEmail().getAttribute("value"));
-	 Assert.assertEquals("Data mismatch in Contact Phone",data.get(1).get(2),adminContacts.getEditPageContactPhone().getAttribute("value"));
-	 Assert.assertEquals("Data mismatch in Contact Company",data.get(1).get(3),adminContacts.getEditPageContactCompany().getAttribute("value"));
-	 Assert.assertEquals("Data mismatch in Support Role",data.get(1).get(4),adminContacts.getEditPageSupportRole().getAttribute("value"));
-	 Assert.assertEquals("Data mismatch in Display Contact",data.get(1).get(5),adminContacts.getEditPageDisplayContact().getAttribute("value"));
-	 Assert.assertEquals("Data mismatch in New Support Role",data.get(1).get(6),adminContacts.getEditPageNewSupportRoleName().getAttribute("value"));
-	 
-	 }
+	@Then("^the following Edit and Create Contacts row content will be correct$")
+	public void the_following_Edit_and_Create_Contacts_row_content_will_be_correct(DataTable dataTable)
+			throws Throwable {
+		List<List<String>> data = dataTable.raw();
+		adminContacts = new AdminContactsPage(driver);
+		Thread.sleep(5000);
+		Assert.assertEquals("Data mismatch in Contact Name", data.get(1).get(0),
+				adminContacts.getEditPageContactName().getAttribute("value"));
+		Assert.assertEquals("Data mismatch in Contact Email", data.get(1).get(1),
+				adminContacts.getEditPageContactEmail().getAttribute("value"));
+		Assert.assertEquals("Data mismatch in Contact Phone", data.get(1).get(2),
+				adminContacts.getEditPageContactPhone().getAttribute("value"));
+		Assert.assertEquals("Data mismatch in Contact Company", data.get(1).get(3),
+				adminContacts.getEditPageContactCompany().getAttribute("value"));
+		Assert.assertEquals("Data mismatch in Support Role", data.get(1).get(4),
+				adminContacts.getEditPageSupportRole().getAttribute("value"));
+		Assert.assertEquals("Data mismatch in Display Contact", data.get(1).get(5),
+				adminContacts.getEditPageDisplayContact().getAttribute("value"));
+		Assert.assertEquals("Data mismatch in New Support Role", data.get(1).get(6),
+				adminContacts.getEditPageNewSupportRoleName().getAttribute("value"));
+
+	}
 
 	@When("^I select the Manage Contact - Delete action$")
 	public void i_select_the_Manage_Contact_Delete_action() throws Throwable {
+
 		adminContacts = new AdminContactsPage(driver);
 		BrowserInteractions.click(adminContacts.getEditPageCancelButton());
-		// BrowserInteractions.clickWhenElementVisible(driver,
-		// adminContacts.getEditPageCancelButton());
+		Thread.sleep(3000);
 		BrowserInteractions.clickWhenElementVisible(driver, adminContacts.getDeleteButton());
 	}
 
@@ -155,6 +169,7 @@ public class scr_087_manage_contact_list {
 	public void i_click_on_the_button(String arg1) throws Throwable {
 		adminContacts = new AdminContactsPage(driver);
 		BrowserInteractions.clickWhenElementVisible(driver, adminContacts.getDeleteButton());
+		Thread.sleep(3000);
 		BrowserInteractions.click(adminContacts.getCancelButton());
 	}
 
@@ -181,5 +196,115 @@ public class scr_087_manage_contact_list {
 		adminContacts = new AdminContactsPage(driver);
 		Select dropdown = new Select(adminContacts.getContactsPageCount());
 		Assert.assertEquals("Default page count is not 20", displayCount, dropdown.getFirstSelectedOption().getText());
+	}
+
+	@When("^I select \"([^\"]*)\" in the Pagination - Manage Contacts select list$")
+	public void i_select_in_the_Pagination_Manage_Contacts_select_list(String pageCount) throws Throwable {
+		adminContacts = new AdminContactsPage(driver);
+		Select dropdown = new Select(adminContacts.getContactsPageCount());
+		dropdown.selectByVisibleText(pageCount);
+	}
+
+	@Then("^the Manage Contacts results count will be as expected$")
+	public void the_Manage_Contacts_results_count_will_be_as_expected() throws Throwable {
+		adminContacts = new AdminContactsPage(driver);
+		Thread.sleep(3000);
+		String totalRecordCounts = adminContacts.getPageCountResultCount().getText().split("of")[1].trim();
+		System.out.println(totalRecordCounts);
+		int totalRecordCount = Integer.parseInt(totalRecordCounts);
+		Select dropdown = new Select(adminContacts.getContactsPageCount());
+		int pageCount = Integer.valueOf(dropdown.getFirstSelectedOption().getText());
+		if (totalRecordCount > pageCount) {
+			Assert.assertEquals("", totalRecordCount, pageCount);
+		} else {
+			Assert.assertTrue("", totalRecordCount <= pageCount);
+		}
+	}
+
+	@When("^I click the Forward Pagination arrow$")
+	public void i_click_the_Forward_Pagination_arrow() throws Throwable {
+		Thread.sleep(3000);
+		adminContacts = new AdminContactsPage(driver);
+		BrowserInteractions.clickWhenElementVisible(driver, adminContacts.getPaginationForwardArrow());
+	}
+
+	@Then("^the Manage Contacts result count after clicking pagination arrow will be as expected$")
+	public void the_Manage_Contacts_result_count_after_clicking_pagination_arrow_will_be_as_expected()
+			throws Throwable {
+		adminContacts = new AdminContactsPage(driver);
+		String forwardPageCounts = adminContacts.getPageCountResultCount().getText().split(" ")[1].trim();
+		int forwardPageCount = Integer.parseInt(forwardPageCounts);
+		adminContacts = new AdminContactsPage(driver);
+		Select dropdown = new Select(adminContacts.getContactsPageCount());
+		int pageCount = Integer.parseInt(dropdown.getFirstSelectedOption().getText());
+		Assert.assertEquals("Result count after clicking pagination is incorrect", pageCount + 1, forwardPageCount);
+	}
+
+	@When("^I click the Backward Pagination arrow$")
+	public void i_click_the_Backward_Pagination_arrow() throws Throwable {
+		Thread.sleep(3000);
+		adminContacts = new AdminContactsPage(driver);
+		BrowserInteractions.click(adminContacts.getPaginationBackwardArrow());
+
+	}
+
+	@Then("^the Manage Contacts result count after clicking backward pagination arrow will be as expected$")
+	public void the_Manage_Contacts_result_count_after_clicking_backward_pagination_arrow_will_be_as_expected()
+			throws Throwable {
+		Select dropdown = new Select(adminContacts.getContactsPageCount());
+		Assert.assertEquals("Result count mismtach", dropdown.getFirstSelectedOption().getText(),
+				adminContacts.getPageCountResultCount().getText().split("to")[1].trim());
+
+	}
+
+	@When("^I navigate to Page (\\d+)$")
+	public void i_navigate_to_Page(int pageNumber) throws Throwable {
+
+		BrowserInteractions.click(driver.findElement(By.id("pager_goto_page_" + pageNumber)));
+
+	}
+
+	@When("^I navigate to Manage Contacts Page (\\d+)$")
+	public void i_navigate_to_Manage_Contacts_Page(int pageNumber) throws Throwable {
+		adminContacts = new AdminContactsPage(driver);
+		BrowserInteractions.click(driver.findElement(By.id("pager_goto_page_" + pageNumber)));
+	}
+
+	@Then("^the Manage Contacts result count on page (\\d+) will be as expected$")
+	public void the_Manage_Contacts_result_count_on_page_will_be_as_expected(int resultCount) throws Throwable {
+
+		String pageCounts = adminContacts.getPageCountResultCount().getText().split("to")[1].trim();
+		int pageCount = Integer.parseInt(pageCounts);
+		Assert.assertEquals("Result count mismtach",
+				adminContacts.getSecondPageResultCount().getText().split(":")[1].trim(), pageCount + 1);
+
+	}
+
+	@Then("^Manage Contacts Page number (\\d+) will be highlighted$")
+	public void manage_Contacts_Page_number_will_be_highlighted(int pageNumber) throws Throwable {
+		adminContacts = new AdminContactsPage(driver);
+		Thread.sleep(3000);
+		Assert.assertEquals("Page " + pageNumber + " is not highlighted", "pageNav current",
+				driver.findElement(By.id("pager_goto_page_" + pageNumber)).getAttribute("class"));
+	}
+
+	@When("^I click on the element \"([^\"]*)\" button$")
+	public void i_click_on_the_element_button(String arg1) throws Throwable {
+		adminContacts = new AdminContactsPage(driver);
+		Thread.sleep(3000);
+		List<WebElement> elements = driver.findElements(By.id("addContactButton"));
+		for (WebElement webElement : elements) {
+			if (webElement.getText().equalsIgnoreCase("CANCEL")) {
+				webElement.click();
+			}
+		}
+	}
+
+	@Then("^I am on the Administration page$")
+	public void i_am_on_the_Administration_page() throws Throwable {
+		adminContacts = new AdminContactsPage(driver);
+		Thread.sleep(5000);
+		Assert.assertTrue("Contacts List Page Loading failed",
+				BrowserInteractions.isDisplayed(adminContacts.getAdminPageFirstTitle()));
 	}
 }
