7617fa5b83aad2d67c2023493fa0329bdd6b2ca9
Meghana <meghana.chappidi@centricconsulting.com>
2017-08-02 16:03:49 -0400

    Added new elements in combined PDFs page

--- src/test/java/pom/CombinedPDFsPage.java
+++ src/test/java/pom/CombinedPDFsPage.java
@@ -34,6 +34,54 @@ public class CombinedPDFsPage {
 		@FindBy(id="displayCount_file")
 		WebElement pageCount;
 		
+		@FindBy(id="jobId")
+		WebElement jobId;
+		
+		@FindBy(id="saveButton")
+		WebElement saveButton;
+		
+		@FindBy(id="ok_button")
+		WebElement okButton;
+		
+		@FindBy(xpath=".//*[@id='prodholds']/tbody/tr[1]/td[1]")
+		WebElement requestDate;
+		
+		@FindBy(xpath=".//*[@id='prodholds']/tbody/tr[1]/td[2]")
+		WebElement description;
+		
+		@FindBy(xpath=".//*[@id='prodholds']/tbody/tr[1]/td[3]")
+		WebElement author;
+		
+		@FindBy(xpath=".//*[@id='prodholds']/tbody/tr[1]/td[4]")
+		WebElement status;
+		
+		@FindBy(xpath=".//*[@id='prodholds']/tbody/tr[1]/td[5]")
+		WebElement results;
+		
+		@FindBy(xpath=".//*[@id='prodholds']/tbody/tr[1]/td[6]")
+		WebElement actions;
+		
+		@FindBy(id="printId")
+		WebElement printID;
+		
+		@FindBy(id="groupId")
+		WebElement groupID;
+		
+		@FindBy(id="planId")
+		WebElement planID;
+		
+		@FindBy(xpath="html/body/app-root/div[3]/addpdfrequest/div[2]/edit-component/div/div[1]/form/div[3]/div/date-range/div/div")
+		WebElement processedDate;
+		
+		@FindBy(id="processedDate_pulldown")
+		WebElement processedDateDropdown;
+		
+		@FindBy(id="processedDate_from_date")
+		WebElement processedDateFromDate;
+		
+		@FindBy(id="processedDate_to_date")
+		WebElement processedDateToDate;
+		
 		public WebElement getResultCount() {
 		return resultCount;
 		}
@@ -88,5 +136,133 @@ public class CombinedPDFsPage {
 
 		public void setPageCount(WebElement pageCount) {
 			this.pageCount = pageCount;
+		}
+
+		public WebElement getJobId() {
+			return jobId;
+		}
+
+		public void setJobId(WebElement jobId) {
+			this.jobId = jobId;
+		}
+
+		public WebElement getSaveButton() {
+			return saveButton;
+		}
+
+		public void setSaveButton(WebElement saveButton) {
+			this.saveButton = saveButton;
+		}
+
+		public WebElement getOkButton() {
+			return okButton;
+		}
+
+		public void setOkButton(WebElement okButton) {
+			this.okButton = okButton;
+		}
+
+		public WebElement getRequestDate() {
+			return requestDate;
+		}
+
+		public void setRequestDate(WebElement requestDate) {
+			this.requestDate = requestDate;
+		}
+
+		public WebElement getDescription() {
+			return description;
+		}
+
+		public void setDescription(WebElement description) {
+			this.description = description;
+		}
+
+		public WebElement getAuthor() {
+			return author;
+		}
+
+		public void setAuthor(WebElement author) {
+			this.author = author;
+		}
+
+		public WebElement getStatus() {
+			return status;
+		}
+
+		public void setStatus(WebElement status) {
+			this.status = status;
+		}
+
+		public WebElement getResults() {
+			return results;
+		}
+
+		public void setResults(WebElement results) {
+			this.results = results;
+		}
+
+		public WebElement getActions() {
+			return actions;
+		}
+
+		public void setActions(WebElement actions) {
+			this.actions = actions;
+		}
+
+		public WebElement getPrintID() {
+			return printID;
+		}
+
+		public void setPrintID(WebElement printID) {
+			this.printID = printID;
+		}
+
+		public WebElement getProcessedDateDropdown() {
+			return processedDateDropdown;
+		}
+
+		public void setProcessedDateDropdown(WebElement processedDateDropdown) {
+			this.processedDateDropdown = processedDateDropdown;
+		}
+
+		public WebElement getGroupID() {
+			return groupID;
+		}
+
+		public void setGroupID(WebElement groupID) {
+			this.groupID = groupID;
+		}
+
+		public WebElement getPlanID() {
+			return planID;
+		}
+
+		public void setPlanID(WebElement planID) {
+			this.planID = planID;
+		}
+
+		public WebElement getProcessedDateFromDate() {
+			return processedDateFromDate;
+		}
+
+		public void setProcessedDateFromDate(WebElement processedDateFromDate) {
+			this.processedDateFromDate = processedDateFromDate;
+		}
+
+		public WebElement getProcessedDateToDate() {
+			return processedDateToDate;
+		}
+
+		public void setProcessedDateToDate(WebElement processedDateToDate) {
+			this.processedDateToDate = processedDateToDate;
+		}
+
+		public WebElement getProcessedDate() {
+			return processedDate;
+		}
+
+		public void setProcessedDate(WebElement processedDate) {
+			this.processedDate = processedDate;
 		}
 }
