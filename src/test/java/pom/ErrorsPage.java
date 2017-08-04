c603b0d82b2e9b9d1771af3ddf69fc8ac1ecde95
Meghana <meghana.chappidi@centricconsulting.com>
2017-08-04 13:35:54 -0400

    Added new elements in POM file of errors page

--- src/test/java/pom/ErrorsPage.java
+++ src/test/java/pom/ErrorsPage.java
@@ -25,7 +25,22 @@ public class ErrorsPage {
 	
 	@FindBy(css=".container-fluid>errors>h3")
 	WebElement errorsTitle;
+	
+	@FindBy(xpath="html/body/app-root/div[3]/errors/div[1]/a[2]")
+	WebElement viewFileDetailsButton;
+	
+	@FindBy(xpath="html/body/app-root/div[3]/errorreport/h3")
+	WebElement ViewReportTitle;
+	
+	@FindBy(id="errors_sort_48")
+	WebElement sortButton;
 		
+	@FindBy(id="displayCount_file")
+	WebElement pageCount;
+	
+	@FindBy(xpath="html/body/app-root/div[3]/file-details/h3")
+	WebElement viewFileDetailsTitle;
+	
 	public WebElement getErrorLink() {
 		return errorLink;
 	}
@@ -58,5 +73,44 @@ public class ErrorsPage {
 		this.errorsTitle = errorsTitle;
 	}
 
+	public WebElement getViewFileDetailsButton() {
+		return viewFileDetailsButton;
 	}
+
+	public void setViewFileDetailsButton(WebElement viewFileDetailsButton) {
+		this.viewFileDetailsButton = viewFileDetailsButton;
+	}
+
+	public WebElement getViewReportTitle() {
+		return ViewReportTitle;
+	}
+
+	public void setViewReportTitle(WebElement viewReportTitle) {
+		ViewReportTitle = viewReportTitle;
+	}
+
+	public WebElement getSortButton() {
+		return sortButton;
+	}
+
+	public void setSortButton(WebElement sortButton) {
+		this.sortButton = sortButton;
+	}
+
+	public WebElement getPageCount() {
+		return pageCount;
+	}
+
+	public void setPageCount(WebElement pageCount) {
+		this.pageCount = pageCount;
+	}
+
+	public WebElement getViewFileDetailsTitle() {
+		return viewFileDetailsTitle;
+	}
+
+	public void setViewFileDetailsTitle(WebElement viewFileDetailsTitle) {
+		this.viewFileDetailsTitle = viewFileDetailsTitle;
+	}
+}
 
